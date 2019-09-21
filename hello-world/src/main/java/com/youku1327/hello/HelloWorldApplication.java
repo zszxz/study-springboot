2eae6b98c028b12955e8f7af63cb5cbb281e4494
lsc <lsc@kingboya.com>
2019-09-22 00:08:24 +0800

    springboot helloworld

---
+++ hello-world/src/main/java/com/youku1327/hello/HelloWorldApplication.java
@@ -0,0 +1,18 @@
+package com.youku1327.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lsc
+ * @Description springboot启动类
+ * @Date 2019/9/21 23:40
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class,args);
+    }
+}
---
+++ hello-world/src/main/java/com/youku1327/hello/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.youku1327.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author lsc
+ * @Description hello world 控制层
+ * @Date 2019/9/21 23:44
+ * @Version 1.0
+ */
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/hello/springboot")
+    @ResponseBody
+    public String quick(){
+        return "公众号 youku1327 hello world springboot!";
+    }
+}
