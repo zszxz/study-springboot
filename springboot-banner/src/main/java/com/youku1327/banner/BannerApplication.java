c312864e6d9e551eadfe8ba718cc15abda3e3023
lsc <lsc@kingboya.com>
2019-10-02 15:48:31 +0800

    springboot 集成 banner

---
+++ springboot-banner/src/main/java/com/youku1327/banner/BannerApplication.java
@@ -0,0 +1,18 @@
+package com.youku1327.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lsc
+ * @Description <p> </p>
+ * @Date 2019/10/2 14:59
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class BannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BannerApplication.class,args);
+    }
+}
